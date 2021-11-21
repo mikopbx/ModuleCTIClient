4b9af0791de5e053bfa3eac98d27968a3c765851
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:52:23 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (27 of 27 strings)
    
    Translation: MIKOPBX/ModuleCTIClient
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulecticlient/sv/

--- Messages/sv.php
+++ Messages/sv.php
@@ -1,1 +1,37 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 9 2018
+ *
+ */
+    'BreadcrumbModuleCTIClient' => 'Telefonipanel 2.0 för 1C',
+    'SubHeaderModuleCTIClient' => 'PC-anropskontrollmodul',
+    'fw_modulecticlientDescription' => 'CTI CLIENT - Telefonipanel 2.0 för 1C',
+    'mod_cti_ValidateServer1CHostEmpty' => '1C-serveradressen är inte ifylld',
+    'mod_cti_ValidateServer1CPortRange' => 'Porten för 1C-servern är inte korrekt angiven',
+    'mod_cti_ValidatePubName' => 'Publiceringsnamnet för 1C-webbtjänsten är inte specificerat',
+    'mod_cti_Server1CHostPort' => '1C-serveradress och port',
+    'mod_cti_Login' => 'Logga in för 1C webbtjänst',
+    'mod_cti_Password' => 'Lösenord för auktorisering i 1C',
+    'mod_cti_PublicationName' => 'Publikationens namn',
+    'mod_cti_UseAutoSettings' => 'Automatisk modulkonfiguration',
+    'mod_cti_PublicationOverHeader' => 'Anslutningsläge med 1C',
+    'mod_cti_PublicationOverWebServices' => 'via webbtjänst (MikoPBX ansluter till 1C)',
+    'mod_cti_PublicationOverLongPool' => 'via LongPool-anslutning (1C ansluter till MikoPBX)',
+    'mod_cti_AuthApp_Name' => 'Extern panelregistrering',
+    'mod_cti_AuthApp_Description' => 'Låter dig binda panelen till SIP-kontot med digital kod',
+    'mod_cti_EnableDebugMode' => 'Aktivera modulfelsökning och loggning av alla händelser',
+    'mod_cti_AutoSettingsData' => 'Data för automatisk konfiguration',
+    'mod_cti_Connected' => 'Modulen fungerar korrekt',
+    'mod_cti_Disconnected' => 'inaktiverad',
+    'mod_cti_ConnectionProgress' => 'Modultjänster startas',
+    'mod_cti_ConnectionError' => 'Fel i modulens drift',
+    'mod_cti_ConnectionTo1CError' => 'Det gick inte att ansluta till 1C-webbtjänsten',
+    'mod_cti_UpdateStatus' => 'Statusuppdatering',
+    'mod_cti_ConnectionWait' => 'Vi väntar på en anslutning till MikoPBX från 1C-sidan',
+    'mod_cti_tab_Settings' => 'inställningar',
+    'mod_cti_tab_debug' => 'Avlusningsmodultjänster',
+];
