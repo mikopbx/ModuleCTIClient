0730cf8d9432faff993f195bf53db26bf42676d0
Nikolay Beketov <nbek@miko.ru>
2021-11-23 17:07:01 +0000

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (27 of 27 strings)
    
    Translation: MIKOPBX/ModuleCTIClient
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulecticlient/ka/

--- Messages/ka.php
+++ Messages/ka.php
@@ -1,1 +1,37 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 9 2018
+ *
+ */
+    'BreadcrumbModuleCTIClient' => 'სატელეფონო პანელი 1.0 1C-სთვის',
+    'SubHeaderModuleCTIClient' => 'მოდული კომპიუტერიდან ზარების მართვისთვის',
+    'fw_modulecticlientDescription' => 'CTI CLIENT - სატელეფონო პანელი 1.0 1C-ისთვის',
+    'mod_cti_ValidateServer1CHostEmpty' => '1C სერვერის მისამართი არ არის შევსებული',
+    'mod_cti_ValidateServer1CPortRange' => '1C სერვერის პორტი სწორად არ არის მითითებული',
+    'mod_cti_ValidatePubName' => '1C ვებ სერვისის პუბლიკაციის სახელი არ არის მითითებული',
+    'mod_cti_Server1CHostPort' => '1C სერვერის მისამართი და პორტი',
+    'mod_cti_Login' => 'შედით 1C ვებ სერვისზე',
+    'mod_cti_Password' => 'ავტორიზაციის პაროლი 1C-ში',
+    'mod_cti_PublicationName' => 'პუბლიკაციის სახელი',
+    'mod_cti_UseAutoSettings' => 'მოდულის ავტომატური კონფიგურაცია',
+    'mod_cti_PublicationOverHeader' => 'კავშირის რეჟიმი 1C-თან',
+    'mod_cti_PublicationOverWebServices' => 'ვებ სერვისის საშუალებით (MikoPBX უკავშირდება 1C-ს)',
+    'mod_cti_PublicationOverLongPool' => 'LongPool კავშირის საშუალებით (1C უკავშირდება MikoPBX-ს)',
+    'mod_cti_AuthApp_Name' => 'გარე პანელის რეგისტრაცია',
+    'mod_cti_AuthApp_Description' => 'საშუალებას გაძლევთ დააკავშიროთ პანელი SIP ანგარიშთან ციფრული კოდით',
+    'mod_cti_EnableDebugMode' => 'ჩართეთ მოდულის გამართვა და ყველა მოვლენის აღრიცხვა',
+    'mod_cti_AutoSettingsData' => 'მონაცემები ავტომატური კონფიგურაციისთვის',
+    'mod_cti_Connected' => 'მოდული მუშაობს გამართულად',
+    'mod_cti_Disconnected' => 'ინვალიდი',
+    'mod_cti_ConnectionProgress' => 'მოდულის სერვისები დაწყებულია',
+    'mod_cti_ConnectionError' => 'მოდულის მუშაობის შეცდომა',
+    'mod_cti_ConnectionTo1CError' => '1C ვებ სერვისთან დაკავშირება ვერ მოხერხდა',
+    'mod_cti_UpdateStatus' => 'Სტატუსის განახლება',
+    'mod_cti_ConnectionWait' => 'ჩვენ ველოდებით MikoPBX-თან კავშირს 1C მხრიდან',
+    'mod_cti_tab_Settings' => 'პარამეტრები',
+    'mod_cti_tab_debug' => 'მოდულის გამართვის სერვისები',
+];
