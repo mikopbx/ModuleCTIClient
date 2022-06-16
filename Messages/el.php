5a034f61065fdc3287f7a17e0c872b4d2a4f4b8c
Voutsas Theocharis <voutsasth@yahoo.gr>
2022-06-16 21:52:07 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (34 of 34 strings)
    
    Translation: MIKOPBX/ModuleCTIClient
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulecticlient/el/

--- Messages/el.php
+++ Messages/el.php
@@ -1,1 +1,45 @@
 <?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 9 2018
+ *
+ */
+    'BreadcrumbModuleCTIClient' => 'Πίνακας τηλεφωνίας 4.0 για 1C',
+    'mod_cti_TransliterateCallerID' => 'Εκτελέστε μεταγραφή του CallerID που ελήφθη από 1C',
+    'mod_cti_EnableSetCallerID' => 'Εγκαταστήστε το CallerID από 1C',
+    'mod_cti_OdinEsSetupMessageStep4' => 'Πατήστε Ctrl+O (ή Αρχείο&#8594;Άνοιγμα) και επιλέξτε το αρχείο "Installer.epf"',
+    'mod_cti_OdinEsSetupMessageStep2' => 'Εκτελέστε το 1C αποκλειστικά σε λειτουργία επιχείρησης',
+    'mod_cti_OdinEsSetupMessageStep3' => 'Κάντε λήψη του προγράμματος εγκατάστασης επέκτασης &nbsp;
+<a href="https://releases.mikopbx.com/releases/v1/1c/getModuleFile/PT40_Installer/latest" target="_blank">Σύνδεσμος <i class="cloud download icon"></i></a>',
+    'mod_cti_OdinEsSetupMessageStep1' => 'Δημιουργήστε ένα αντίγραφο ασφαλείας της βάσης δεδομένων 1C',
+    'mod_cti_OdinEsSetupHeaderMessage' => 'Εγκαταστήστε το υποσύστημα τηλεφωνίας στο 1C:Enterprise 8',
+    'mod_cti_tab_debug' => 'Υπηρεσίες μονάδας εντοπισμού σφαλμάτων',
+    'mod_cti_tab_Settings' => 'Ρυθμίσεις',
+    'mod_cti_ConnectionWait' => 'Περιμένουμε σύνδεση στο MikoPBX από 1C',
+    'mod_cti_UpdateStatus' => 'Ενημέρωση κατάστασης',
+    'mod_cti_ConnectionTo1CError' => 'Όχι, δεν ήταν δυνατή η σύνδεση στην υπηρεσία web 1C',
+    'mod_cti_ConnectionError' => 'Σφάλμα λειτουργίας μονάδας',
+    'mod_cti_ConnectionProgress' => 'Ξεκινούν οι υπηρεσίες της ενότητας',
+    'mod_cti_Disconnected' => 'Άτομα με ειδικές ανάγκες',
+    'mod_cti_Connected' => 'Η μονάδα λειτουργεί σωστά',
+    'mod_cti_AutoSettingsData' => 'Δεδομένα για αυτόματο συντονισμό',
+    'mod_cti_EnableDebugMode' => 'Ενεργοποιήστε τη λειτουργία εντοπισμού σφαλμάτων της μονάδας και την καταγραφή όλων των συμβάντων',
+    'mod_cti_AuthApp_Description' => 'Σας επιτρέπει να συνδέσετε τον πίνακα σε έναν λογαριασμό SIP χρησιμοποιώντας έναν ψηφιακό κωδικό',
+    'mod_cti_AuthApp_Name' => 'Καταχώρηση εξωτερικού πίνακα',
+    'mod_cti_PublicationOverLongPool' => 'Μέσω σύνδεσης LongPool (1C συνδέεται στο MikoPBX)',
+    'mod_cti_PublicationOverWebServices' => 'Μέσω υπηρεσίας web (το MikoPBX συνδέεται στο 1C)',
+    'mod_cti_PublicationOverHeader' => 'Λειτουργία σύνδεσης με 1C',
+    'mod_cti_UseAutoSettings' => 'Αυτόματη διαμόρφωση των παραμέτρων επικοινωνίας της μονάδας και 1C',
+    'mod_cti_PublicationName' => 'Όνομα δημοσίευσης',
+    'mod_cti_Password' => 'Κωδικός πρόσβασης για εξουσιοδότηση σε 1C',
+    'mod_cti_Login' => 'Είσοδος για την υπηρεσία web 1C',
+    'mod_cti_Server1CHostPort' => 'Διεύθυνση διακομιστή και θύρα 1C',
+    'mod_cti_ValidatePubName' => 'Το όνομα της δημοσίευσης της υπηρεσίας web 1C δεν προσδιορίζεται',
+    'mod_cti_ValidateServer1CPortRange' => 'Η θύρα του διακομιστή 1C δεν έχει καθοριστεί σωστά',
+    'mod_cti_ValidateServer1CHostEmpty' => 'Η διεύθυνση διακομιστή 1C δεν έχει συμπληρωθεί',
+    'fw_modulecticlientDescription' => 'CTI CLIENT - Πίνακας τηλεφωνίας 4.0 για 1C',
+    'SubHeaderModuleCTIClient' => 'Μονάδα διαχείρισης κλήσεων από υπολογιστή',
+];
