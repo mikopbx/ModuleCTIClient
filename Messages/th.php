201d6d9f31a018d171b3175e8c46b827ec8f3424
SUMONCHAI WONGPITAK <sumonchai@gmail.com>
2024-06-07 03:46:56 +0000

    Translated using Weblate (Thai)
    Currently translated at 28.5% (10 of 35 strings)
    
    Translation: MIKOPBX/ModuleCTIClient
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulecticlient/th/

--- Messages/th.php
+++ Messages/th.php
@@ -1,1 +1,32 @@
 <?php
+return [
+    /*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+    'BreadcrumbModuleCTIClient' => 'Telephony panel 4.0 for 1C',
+    'SubHeaderModuleCTIClient' => 'โมดูลสําหรับจัดการการโทรจากคอมพิวเตอร์',
+    'fw_modulecticlientDescription' => 'CTI CLIENT - Telephony panel 4.0 for 1C',
+    'mod_cti_ValidateServer1CHostEmpty' => 'The Server 1C:Enterprise host field is empty',
+    'mod_cti_ValidateServer1CPortRange' => 'The Server 1C:Enterprise port field is out-of-range',
+    'mod_cti_ValidatePubName' => 'ไม่ได้ระบุชื่อสิ่งพิมพ์ของบริการเว็บ 1C',
+    'mod_cti_Server1CHostPort' => 'Server 1C:Enterprise host and port',
+    'mod_cti_Login' => 'เข้าสู่ระบบสําหรับบริการเว็บ 1C',
+    'mod_cti_Password' => 'รหัสผ่านสําหรับการอนุญาตใน 1C',
+    'mod_cti_PublicationName' => 'ชื่อสิ่งพิมพ์',
+    'mod_cti_PublicationNameForAuth' => 'ชื่อสิ่งพิมพ์ที่มีการรับรองความถูกต้องของระบบปฏิบัติการ (ไม่บังคับ)',
+    'mod_cti_UseAutoSettings' => 'การกำหนดค่าอัตโนมัติของโมดูลและพารามิเตอร์การสื่อสาร 1C',
+];
