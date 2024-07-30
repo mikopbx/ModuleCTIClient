b1b6ce79ac05bb7e772666aab74e486d1dfef03c
Juhász Béla <korfa@freemail.hu>
2024-07-30 12:41:50 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 34.2% (12 of 35 strings)
    
    Translation: MIKOPBX/ModuleCTIClient
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulecticlient/hu/

--- Messages/hu.php
+++ Messages/hu.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'fw_modulecticlientDescription' => 'CTI CLIENT - Telefonos panel 4.0 az 1C számára',
+    'mod_cti_ValidateServer1CHostEmpty' => 'Az 1C szerver címe nincs kitöltve',
+    'mod_cti_Server1CHostPort' => 'Az 1C szerver címe és portja',
+    'mod_cti_Password' => 'Jelszó az engedélyezéshez az 1C-ben',
+    'mod_cti_PublicationNameForAuth' => 'Kiadványnév operációs rendszer hitelesítéssel (opcionális)',
+    'mod_cti_UseAutoSettings' => 'A modul és az 1C kommunikációs paraméterek automatikus beállítása',
+    'mod_cti_PublicationOverWebServices' => 'webes szolgáltatáson keresztül (a MikoPBX csatlakozik az 1C-hez)',
+    'mod_cti_ValidateServer1CPortRange' => 'Az 1C szerverport helytelenül van megadva',
+    'mod_cti_ValidatePubName' => 'Az 1C webszolgáltatás közzétételi neve nincs megadva',
+    'mod_cti_Login' => 'Bejelentkezés az 1C webes szolgáltatáshoz',
+    'mod_cti_PublicationName' => 'Kiadvány neve',
+    'mod_cti_PublicationOverHeader' => 'Csatlakozási mód 1C-vel',
+];
