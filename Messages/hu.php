3c86458aee70b8388b3c08f67236cf1a9a9b1864
Juhász Béla <korfa@freemail.hu>
2024-08-22 07:12:14 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (35 of 35 strings)
    
    Translation: MIKOPBX/ModuleCTIClient
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulecticlient/hu/

--- Messages/hu.php
+++ Messages/hu.php
@@ -1,15 +1,55 @@
 <?php
 return [
-    'fw_modulecticlientDescription' => 'CTI CLIENT - Telefonos panel 4.0 az 1C számára',
+    'fw_modulecticlientDescription' => 'CTI CLIENT - Telefonpanel 4.0 1C-hez',
     'mod_cti_ValidateServer1CHostEmpty' => 'Az 1C szerver címe nincs kitöltve',
-    'mod_cti_Server1CHostPort' => 'Az 1C szerver címe és portja',
+    'mod_cti_Server1CHostPort' => '1C szerver címe és portja',
     'mod_cti_Password' => 'Jelszó az engedélyezéshez az 1C-ben',
-    'mod_cti_PublicationNameForAuth' => 'Kiadványnév operációs rendszer hitelesítéssel (opcionális)',
-    'mod_cti_UseAutoSettings' => 'A modul és az 1C kommunikációs paraméterek automatikus beállítása',
-    'mod_cti_PublicationOverWebServices' => 'webes szolgáltatáson keresztül (a MikoPBX csatlakozik az 1C-hez)',
+    'mod_cti_PublicationNameForAuth' => 'Kiadvány neve operációs rendszer hitelesítéssel (opcionális)',
+    'mod_cti_UseAutoSettings' => 'A modul és az 1C kommunikációs paraméterek automatikus konfigurálása',
+    'mod_cti_PublicationOverWebServices' => 'webszolgáltatáson keresztül (MikoPBX csatlakozik az 1C-hez)',
     'mod_cti_ValidateServer1CPortRange' => 'Az 1C szerverport helytelenül van megadva',
     'mod_cti_ValidatePubName' => 'Az 1C webszolgáltatás közzétételi neve nincs megadva',
-    'mod_cti_Login' => 'Bejelentkezés az 1C webes szolgáltatáshoz',
+    'mod_cti_Login' => 'Bejelentkezés az 1C webszolgáltatáshoz',
     'mod_cti_PublicationName' => 'Kiadvány neve',
     'mod_cti_PublicationOverHeader' => 'Csatlakozási mód 1C-vel',
+    /*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+    'BreadcrumbModuleCTIClient' => 'Telefonpanel 4.0 1C-hez',
+    'SubHeaderModuleCTIClient' => 'Modul a hívások kezelésére számítógépről',
+    'mod_cti_PublicationOverLongPool' => 'LongPool kapcsolaton keresztül (1C csatlakozik a MikoPBX-hez)',
+    'mod_cti_AuthApp_Name' => 'Külső panel regisztrálása',
+    'mod_cti_AuthApp_Description' => 'Lehetővé teszi a panel összekapcsolását egy SIP-fiókkal digitális kód segítségével',
+    'mod_cti_EnableDebugMode' => 'Engedélyezze a modul hibakeresési módját és az összes esemény naplózását',
+    'mod_cti_AutoSettingsData' => 'Automatikus konfigurációs adatok',
+    'mod_cti_Connected' => 'A modul megfelelően működik',
+    'mod_cti_Disconnected' => 'Tiltva',
+    'mod_cti_ConnectionProgress' => 'A modulszolgáltatások elindulnak',
+    'mod_cti_ConnectionError' => 'Modul működési hiba',
+    'mod_cti_ConnectionTo1CError' => 'Nem, nem lehetett csatlakozni az 1C webszolgáltatáshoz',
+    'mod_cti_UpdateStatus' => 'Állapotfrissítés',
+    'mod_cti_ConnectionWait' => '1C oldalról várjuk a csatlakozást a MikoPBX-hez',
+    'mod_cti_tab_Settings' => 'Beállítások',
+    'mod_cti_tab_debug' => 'Hibakeresési modul szolgáltatások',
+    'mod_cti_OdinEsSetupHeaderMessage' => 'Telepítse a telefonos alrendszert az 1C:Enterprise 8-ban',
+    'mod_cti_OdinEsSetupMessageStep1' => 'Készítsen biztonsági másolatot az 1C adatbázisáról',
+    'mod_cti_OdinEsSetupMessageStep3' => 'Töltse le az &nbsp;<a href="https://releases.mikopbx.com/releases/v1/1c/getModuleFile/PT40_Installer/latest" target="_blank"> bővítménytelepítőt a következő linkről: <i class="cloud download icon"></i></a>',
+    'mod_cti_OdinEsSetupMessageStep2' => 'Futtassa az 1C-t kizárólag vállalati módban',
+    'mod_cti_OdinEsSetupMessageStep4' => 'Nyomja meg a Ctrl+O (vagy a Fájl&#8594;Open) billentyűkombinációt, és válassza ki az "Installer.epf" fájlt.',
+    'mod_cti_EnableSetCallerID' => 'Telepítse a hívóazonosítót az 1C-ből',
+    'mod_cti_TransliterateCallerID' => 'Végezze el az 1C-től kapott hívóazonosító átírását',
 ];
