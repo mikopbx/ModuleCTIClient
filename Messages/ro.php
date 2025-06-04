522f301d9ac5d8fad2a5a7ce13d24f5c41337b49
Nikolay Beketov <nbek@miko.ru>
2025-06-04 15:04:30 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (35 of 35 strings)
    
    Translation: MIKOPBX/ModuleCTIClient
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulecticlient/ro/

--- Messages/ro.php
+++ Messages/ro.php
@@ -1,1 +1,55 @@
 <?php
+return [
+    'SubHeaderModuleCTIClient' => 'Modul pentru gestionarea apelurilor de pe un computer',
+    'mod_cti_ValidateServer1CPortRange' => 'Portul serverului 1C nu este specificat corect',
+    'mod_cti_Server1CHostPort' => 'Adresa serverului și portul 1C',
+    'mod_cti_AuthApp_Name' => 'Înregistrarea unui panou extern',
+    'mod_cti_AuthApp_Description' => 'Vă permite să conectați panoul la un cont SIP folosind un cod digital',
+    'mod_cti_AutoSettingsData' => 'Date pentru configurare automată',
+    'mod_cti_ConnectionWait' => 'Așteptăm conexiunea la MikoPBX de la 1C',
+    'mod_cti_tab_Settings' => 'Setări',
+    'mod_cti_tab_debug' => 'Servicii de depanare a modulelor',
+    'mod_cti_OdinEsSetupHeaderMessage' => 'Instalați subsistemul de telefonie în 1C:Enterprise 8',
+    'mod_cti_OdinEsSetupMessageStep2' => 'Rulați 1C exclusiv în modul Enterprise',
+    'mod_cti_OdinEsSetupMessageStep4' => 'Apăsați Ctrl+O (sau File’Open) și selectați fișierul „Installer.epf”',
+    /*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+    'BreadcrumbModuleCTIClient' => 'Panou de telefonie 4.0 pentru 1C',
+    'fw_modulecticlientDescription' => 'CLIENT CTI - Panou de telefonie 4.0 pentru 1C',
+    'mod_cti_ValidateServer1CHostEmpty' => 'Adresa serverului 1C nu este completată',
+    'mod_cti_ValidatePubName' => 'Numele publicării serviciului web 1C nu este specificat',
+    'mod_cti_Login' => 'Conectați-vă pentru serviciul web 1C',
+    'mod_cti_Password' => 'Parola pentru autorizare în 1C',
+    'mod_cti_PublicationName' => 'Numele publicației',
+    'mod_cti_PublicationNameForAuth' => 'Numele publicației cu autentificarea sistemului de operare (opțional)',
+    'mod_cti_UseAutoSettings' => 'Configurarea automată a parametrilor de comunicare între modul și 1C',
+    'mod_cti_PublicationOverHeader' => 'Mod de conectare cu 1C',
+    'mod_cti_PublicationOverWebServices' => 'prin serviciu web (MikoPBX se conectează la 1C)',
+    'mod_cti_PublicationOverLongPool' => 'prin conexiunea LongPool (1C se conectează la MikoPBX)',
+    'mod_cti_EnableDebugMode' => 'Activează modul de depanare a modulului și înregistrarea în jurnal a tuturor evenimentelor',
+    'mod_cti_Connected' => 'Modulul funcționează corect.',
+    'mod_cti_Disconnected' => 'Dezactivat',
+    'mod_cti_ConnectionProgress' => 'Serviciile modulelor încep',
+    'mod_cti_ConnectionError' => 'Eroare de funcționare a modulului',
+    'mod_cti_ConnectionTo1CError' => 'Nu este posibilă conexiunea la serviciul web 1C',
+    'mod_cti_UpdateStatus' => 'Actualizarea stării',
+    'mod_cti_OdinEsSetupMessageStep1' => 'Faceți o copie de rezervă a bazei de date 1C',
+    'mod_cti_OdinEsSetupMessageStep3' => 'Descărcați programul de instalare a extensiei &nbsp;<a href="https://releases.mikopbx.com/releases/v1/1c/getModuleFile/PT40_Installer/latest" target="_blank"> de la linkul <i class="cloud download icon"></i></a>',
+    'mod_cti_EnableSetCallerID' => 'Instalați CallerID din 1C',
+    'mod_cti_TransliterateCallerID' => 'Efectuați transliterarea ID-ului apelantului primit de la 1C',
+];
