5845631706478ae188957706f3ab05ed8fccec4b
Nikolay Beketov <nbek@miko.ru>
2025-06-04 15:04:49 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (35 of 35 strings)
    
    Translation: MIKOPBX/ModuleCTIClient
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulecticlient/hr/

--- Messages/hr.php
+++ Messages/hr.php
@@ -1,1 +1,55 @@
 <?php
+return [
+    'mod_cti_ValidateServer1CPortRange' => 'Port poslužitelja 1C je pogrešno naveden',
+    'mod_cti_Server1CHostPort' => 'Adresa i port 1C poslužitelja',
+    'mod_cti_PublicationOverWebServices' => 'putem web servisa (MikoPBX se povezuje s 1C)',
+    'mod_cti_AutoSettingsData' => 'Podaci za automatsku konfiguraciju',
+    'mod_cti_tab_Settings' => 'postavke',
+    'mod_cti_tab_debug' => 'Usluge modula za otklanjanje pogrešaka',
+    'mod_cti_OdinEsSetupMessageStep2' => 'Pokrenite 1C isključivo u poslovnom načinu rada',
+    'mod_cti_OdinEsSetupMessageStep4' => 'Pritisnite Ctrl+O (ili Datoteka - Otvori) i odaberite datoteku "Installer.epf"',
+    'mod_cti_EnableSetCallerID' => 'Instalirajte CallerID iz 1C',
+    'mod_cti_TransliterateCallerID' => 'Izvršite transliteraciju CallerID-a primljenog od 1C',
+    /*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+    'BreadcrumbModuleCTIClient' => 'Telefonska ploča 4.0 za 1C',
+    'SubHeaderModuleCTIClient' => 'Modul za upravljanje pozivima s računala',
+    'fw_modulecticlientDescription' => 'CTI KLIJENT - Telefonska ploča 4.0 za 1C',
+    'mod_cti_ValidateServer1CHostEmpty' => 'Adresa poslužitelja 1C nije popunjena',
+    'mod_cti_ValidatePubName' => 'Naziv publikacije web usluge 1C nije naveden',
+    'mod_cti_Login' => 'Prijava za web uslugu 1C',
+    'mod_cti_Password' => 'Lozinka za autorizaciju u 1C',
+    'mod_cti_PublicationName' => 'Naziv publikacije',
+    'mod_cti_PublicationNameForAuth' => 'Naziv publikacije s provjerom autentičnosti OS-a (nije obavezno)',
+    'mod_cti_UseAutoSettings' => 'Automatska konfiguracija komunikacijskih parametara između modula i 1C',
+    'mod_cti_PublicationOverHeader' => 'Način povezivanja s 1C',
+    'mod_cti_PublicationOverLongPool' => 'putem LongPool veze (1C se spaja na MikoPBX)',
+    'mod_cti_AuthApp_Name' => 'Registriranje vanjske ploče',
+    'mod_cti_AuthApp_Description' => 'Omogućuje vam povezivanje panela sa SIP računom pomoću digitalnog koda',
+    'mod_cti_EnableDebugMode' => 'Omogući način rada za otklanjanje pogrešaka u modulu i zapisivanje svih događaja',
+    'mod_cti_Connected' => 'Modul radi ispravno.',
+    'mod_cti_Disconnected' => 'Onesposobljeno',
+    'mod_cti_ConnectionProgress' => 'Počinju usluge modula',
+    'mod_cti_ConnectionError' => 'Greška u radu modula',
+    'mod_cti_ConnectionTo1CError' => 'Nije moguće povezivanje s 1C web servisom',
+    'mod_cti_UpdateStatus' => 'Ažuriranje statusa',
+    'mod_cti_ConnectionWait' => 'Čekamo vezu s MikoPBX-om od 1C',
+    'mod_cti_OdinEsSetupHeaderMessage' => 'Instalirajte telefonski podsustav u 1C:Enterprise 8',
+    'mod_cti_OdinEsSetupMessageStep1' => 'Napravite sigurnosnu kopiju svoje 1C baze podataka',
+    'mod_cti_OdinEsSetupMessageStep3' => 'Preuzmite instalacijski program za proširenje &nbsp;<a href="https://releases.mikopbx.com/releases/v1/1c/getModuleFile/PT40_Installer/latest" target="_blank"> s poveznice <i class="cloud download icon"></i></a>',
+];
